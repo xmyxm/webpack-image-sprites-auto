967f712bb082ad2bb2e65595dbf3f4153be8f7a4
chenxuanfeng <chenxuanfeng@meituan.com>
2019-08-23 22:09:12 +0800

    up

--- postcss-sprites.config.js
+++ postcss-sprites.config.js
@@ -9,7 +9,7 @@
  *  */
 var postcss = require('postcss');
 // 合法的散列图path 查找 /src/icons/*/*.png
-const REG_SPRITES_PATH = /\/icons\/(.*?)\/.*/i
+const REG_SPRITES_PATH = /\/icons\/.*/i
 // 合法的retina标识 @*x
 const REG_SPRITES_RETINA = /@(\d+)x\./i
 // split
@@ -19,47 +19,48 @@ const RETINA = true
 const OUTPUT_PATH = "./dist/sptite"
 
 module.exports = {
+    // 保存输出spritesheet的文件夹的相对路径
     spritePath: OUTPUT_PATH,
     //过滤 除了 icons 以外 的图片链接
     //需返回一个Promise对象
-    filterBy: (image) => {
-        return REG_SPRITES_PATH.test(image.url) ? Promise.resolve() : Promise.reject()
-    },
-    //分组 分出 1 / 2 / 3 / n倍 图
-    groupBy: (image) => {
-        let groups = null;
-        let groupName = '';
-        let module = image.originalUrl.split(/\?/)
-        image.url = module[0]
-        if (module.length > 1) {
-            //分模块 ? 跟模块名
-            groupName = module[1] + '-'
-        }
-        if (SPLIT) {
-            groups = REG_SPRITES_PATH.exec(image.url);
-            groupName += groups ? groups[1] : 'icons';
-        } else {
-            groupName += 'icons';
-        }
-        //处理多倍图的情况
-        if (RETINA) {
-            image.retina = RETINA;
-            image.ratio = 1;
-            let ratio = REG_SPRITES_RETINA.exec(image.url);
-            if (ratio) {
-                ratio = ratio[1];
-                while (ratio > 10) {
-                    ratio = ratio / 10;
-                }
-                image.ratio = ratio;
-                image.groups = image.groups.filter((group) => {
-                    return ('@' + ratio + 'x') !== group;
-                });
-                groupName += '@' + ratio + 'x';
-            }
-        }
-        return Promise.resolve(groupName);
-    },
+    // filterBy: (image) => {
+    //     return REG_SPRITES_PATH.test(image.url) ? Promise.resolve() : Promise.reject()
+    // },
+    // //分组 分出 1 / 2 / 3 / n倍 图
+    // groupBy: (image) => {
+    //     let groups = null;
+    //     let groupName = '';
+    //     let module = image.originalUrl.split(/\?/)
+    //     image.url = module[0]
+    //     if (module.length > 1) {
+    //         //分模块 ? 跟模块名
+    //         groupName = module[1] + '-'
+    //     }
+    //     if (SPLIT) {
+    //         groups = REG_SPRITES_PATH.exec(image.url);
+    //         groupName += groups ? groups[1] : 'icons';
+    //     } else {
+    //         groupName += 'icons';
+    //     }
+    //     //处理多倍图的情况
+    //     if (RETINA) {
+    //         image.retina = RETINA;
+    //         image.ratio = 1;
+    //         let ratio = REG_SPRITES_RETINA.exec(image.url);
+    //         if (ratio) {
+    //             ratio = ratio[1];
+    //             while (ratio > 10) {
+    //                 ratio = ratio / 10;
+    //             }
+    //             image.ratio = ratio;
+    //             image.groups = image.groups.filter((group) => {
+    //                 return ('@' + ratio + 'x') !== group;
+    //             });
+    //             groupName += '@' + ratio + 'x';
+    //         }
+    //     }
+    //     return Promise.resolve(groupName);
+    // },
     // 转换百分比定位
     hooks: {
         onUpdateRule: function (rule, token, image) {
